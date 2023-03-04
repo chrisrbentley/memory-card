7e11a20cb5d26646cef9100d917d9b66959cab9f
Chris Bentley <chrisrbentley@pm.me>
2023-03-03 22:51:16 -0500

    Created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Baseball Memory</h1>
+      <h3>Score: 0</h3>
+    </header>
+  );
+}
+
+export default Header;
